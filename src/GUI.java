817888096bd9d7a4398a47da3fe61b2752325263
Ninib Masso <ninibmasso@hotmail.com>
2023-10-27 14:51:32 +0200

    Skapat metoden revalidate ändrat i metoden skapaLista.

--- src/GUI.java
+++ src/GUI.java
@@ -1,50 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class GUI extends JFrame {
 
     JPanel jp = new JPanel(new BorderLayout());
     JPanel gridPanel = new JPanel(new GridLayout(4, 4));
 
+    public void revalidate(ArrayList<JButton> buttons){
+        gridPanel.removeAll();
+        for (JButton button : buttons){
+            gridPanel.add(button);
+        }
+        Collections.shuffle(buttons);
+    }
 
-    public void SkapaLista(){
-
+    public ArrayList<JButton> skapaLista() {
         ArrayList<JButton> buttons = new ArrayList<>();
-
-
-
-        for(int i = 0; i<16; i++){
+        for (int i = 0; i < 16; i++) {
             JButton button;
-            if(i == 0){
+            if (i == 0) {
                 button = new JButton("");
-
-            }
-            else{
+            } else {
                 button = new JButton("" + i);
-
             }
             buttons.add(button);
         }
-
-        for (JButton button : buttons) {
-            gridPanel.add(button);
-        }
-
-
+        return buttons;
     }
 
-    GUI(){
+    GUI() {
         this.add(jp);
         jp.add(gridPanel, BorderLayout.SOUTH);
-        SkapaLista();
-
-
+        ArrayList<JButton> buttons = skapaLista();
+        revalidate(buttons);
         pack();
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
     }
 
 }
