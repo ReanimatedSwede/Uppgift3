925e5bada4e64ae0bb28aba19a17ae13bcf2633d
Thomas Olofsson <t.olofsson85@gmail.com>
2023-10-27 14:01:55 +0200

    Skapat en metod för knapplista i GUI

--- src/GUI.java
+++ src/GUI.java
@@ -6,26 +6,39 @@ public class GUI extends JFrame {
 
     JPanel jp = new JPanel(new BorderLayout());
     JPanel gridPanel = new JPanel(new GridLayout(4, 4));
-    JButton button1 = new JButton("1");
-    JButton button2 = new JButton("2");
-    JButton button3 = new JButton("3");
-    JButton button4 = new JButton("4");
-    JButton button5 = new JButton("5");
-    JButton button6 = new JButton("6");
-    JButton button7 = new JButton("7");
-    JButton button8 = new JButton("8");
-    JButton button9 = new JButton("9");
-    JButton button10 = new JButton("10");
-    JButton button11 = new JButton("11");
-    JButton button12 = new JButton("12");
-    JButton button13 = new JButton("13");
-    JButton button14 = new JButton("14");
-    JButton button15 = new JButton("15");
-    JButton button16 = new JButton(" ");
+
+
+    public void SkapaLista(){
+
+        ArrayList<JButton> buttons = new ArrayList<>();
+
+
+
+        for(int i = 0; i<16; i++){
+            JButton button;
+            if(i == 0){
+                button = new JButton("");
+
+            }
+            else{
+                button = new JButton("" + i);
+
+            }
+            buttons.add(button);
+        }
+
+        for (JButton button : buttons) {
+            gridPanel.add(button);
+        }
+
+
+    }
 
     GUI(){
         this.add(jp);
         jp.add(gridPanel, BorderLayout.SOUTH);
+        SkapaLista();
+
 
         pack();
         setVisible(true);
